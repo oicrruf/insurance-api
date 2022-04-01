a05637fb99bb2d8617786387a6ad721594d4c441
ceroncmcr <ceron.cmcr@gmail.com>
2022-04-01 12:04:42 -0600

    chore: rename test health

--- test/health.test.js
+++ test/health.test.js
@@ -3,19 +3,19 @@ const app = require('../app');
 
 const api = supertest(app);
 
-describe('test route /healt', () => {
+describe('/health', () => {
     test('is status code 200', async () => {
         await api.get('/health')
             .expect(200);
     });
 
-    test('is ContentType json', async () => {
+    test('is contenttype json', async () => {
         await api.get('/health')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
     });
 
-    test('is response data it ok', async () => {
+    test('to have name and version properties', async () => {
         const res = await api.get('/health');        
         expect(res.body).toHaveProperty('name');
         expect(res.body).toHaveProperty('version');  
